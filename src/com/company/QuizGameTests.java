39cb533f729fe8d6a14185eb39a3709671f2c897
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-10-24 17:12:06 +0200

    add unit test

---
+++ src/com/company/QuizGameTests.java
@@ -0,0 +1,74 @@
+package com.company;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QuizGameTests {
+
+    @Test
+    void apiControllerGetQuizzesReturnsCorrectObject() throws Exception {
+        // Arrange
+        var fakeController = new FakeQuizApiController("testData/test_response.json");
+
+        // Act
+        QuizRootResponseModel responseQuizRoot = fakeController.getQuizzes(0, 0, null);
+        QuizResponseModel[] responseQuizzes = responseQuizRoot.getResults();
+
+        // Assert
+        assertEquals(0, responseQuizRoot.getResponseCode());
+
+        // Quiz 1
+        assertQuizResponse(responseQuizzes[0],
+                "Science: Computers",
+                "multiple",
+                "medium",
+                "Which programming language was developed by Sun Microsystems in 1995?",
+                "Java",
+                // Incorrect answers
+                "Python",
+                "Solaris OS",
+                "C++");
+
+        // Quiz 2
+        assertQuizResponse(responseQuizzes[1],
+                "Science: Computers",
+                "multiple",
+                "hard",
+                "According to DeMorgan&#039;s Theorem, the Boolean expression (AB)&#039; is equivalent to:",
+                "A&#039; + B&#039;",
+                // Incorrect answers
+                "A&#039;B + B&#039;A",
+                "A&#039;B&#039;",
+                "AB&#039; + AB");
+    }
+
+    private void assertQuizResponse(QuizResponseModel response, String... expectedValues) {
+        assertEquals(expectedValues[0], response.getCategory());
+        assertEquals(expectedValues[1], response.getType());
+        assertEquals(expectedValues[2], response.getDifficulty());
+        assertEquals(expectedValues[3], response.getQuestion());
+        assertEquals(expectedValues[4], response.getCorrectAnswer());
+
+        assertEquals(expectedValues[5], response.getIncorrectAnswers()[0]);
+        assertEquals(expectedValues[6], response.getIncorrectAnswers()[1]);
+        assertEquals(expectedValues[7], response.getIncorrectAnswers()[2]);
+    }
+}
+
+class FakeQuizApiController implements IQuizApiController {
+    private final String jsonString;
+
+    public FakeQuizApiController(String testFilePath) throws IOException {
+        jsonString = Files.readString(Path.of(testFilePath));
+    }
+
+    public QuizRootResponseModel getQuizzes(int amount, int category, String type) throws Exception {
+        return new Gson().fromJson(jsonString, QuizRootResponseModel.class);
+    }
+}
