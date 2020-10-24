bdbfcbfb717db4c0e9a040d26b644765480e3e3c
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-10-24 18:15:25 +0200

    implement game logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var game = new QuizGame();
+        game.start();
     }
 }
