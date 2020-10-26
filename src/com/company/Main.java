47e3ce2bd34d3b5b1765f711e97b83d26ee4e054
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-10-26 18:22:51 +0100

    refactor main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int quizCount = tryPromptInteger("Please enter amount of quizzes: ", "Only integers allowed!");
+        int quizCount = promptUntilInteger("Please enter amount of quizzes: ", "Only integers allowed!");
 
         QuizGameEngine game = null;
         try {
@@ -27,25 +27,15 @@ public class Main {
                 System.out.printf("[%s]: %s\n", i, game.getChoices()[i]);
             }
 
-            int choiceIndex = 0;
-            boolean isInputAccepted = false;
-            while (!isInputAccepted) {
-                choiceIndex = tryPromptInteger("Please enter a choice: ", "Only integers allowed");
-                if (choiceIndex < 0 || choiceIndex >= game.getChoices().length) {
-                    System.out.println("Only integers between 0 and 3 allowed");
-                    continue;
-                }
-                isInputAccepted = true;
-            }
+            int choiceIndex = promptUntilIntegerRange(0, game.getChoices().length, "Please enter a choice: ");
 
             boolean answeredCorrectly = game.isCorrectAnswer(choiceIndex);
 
-            if (answeredCorrectly) {
-                System.out.println("You answered correctly!");
-            } else {
-                System.out.println("You answered incorrectly!");
-            }
+            if (answeredCorrectly) System.out.println("You answered correctly!");
+            else System.out.println("You answered incorrectly!");
+
             System.out.printf("The correct answer is: \"%s\"\n", game.getCorrectAnswer());
+            System.out.println();
 
             game.nextRound(answeredCorrectly);
         }
@@ -53,24 +43,38 @@ public class Main {
         System.out.printf("Score: %s/%s\n", game.getScore(), game.getQuizLength());
     }
 
-    private static int tryPromptInteger(String message, String messageOnFail) {
+    private static int promptUntilInteger(String message, String messageOnFail) {
         Integer value = null;
-        while (value == null) {
+
+         do {
             try {
                 value = Integer.parseInt(prompt(message));
             } catch (NumberFormatException e) {
                 System.out.println(messageOnFail);
             }
-        }
+        } while (value == null);
+
         return value;
     }
 
-    private static String readLine() {
-        return scanner.nextLine();
+    private static int promptUntilIntegerRange(int lower, int upper /*exclusive*/, String message) {
+        boolean isInputAccepted = false;
+        int value;
+
+        do {
+            value = promptUntilInteger(message, "Only integers allowed!");
+            if (value >= lower && value < upper) {
+                isInputAccepted = true;
+            } else {
+                System.out.printf("Value does not fall between >=%s and <%s.\n", lower, upper);
+            }
+        } while (!isInputAccepted);
+
+        return value;
     }
 
     private static String prompt(String message) {
         System.out.print(message);
-        return readLine();
+        return scanner.nextLine();
     }
 }
