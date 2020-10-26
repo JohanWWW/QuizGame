91084418f6e5494b57c7ded8c241ec6d78f4ad65
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-10-26 17:52:43 +0100

    remove obsolete QuizGame which is replaced by code in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,76 @@
 package com.company;
 
-import quiz.QuizGame;
+import quiz.QuizGameEngine;
+import quiz.QuizProviderFailedException;
+
+import java.util.Scanner;
 
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        var game = new QuizGame();
-        game.start();
+        int quizCount = tryPromptInteger("Please enter amount of quizzes: ", "Only integers allowed!");
+
+        QuizGameEngine game = null;
+        try {
+            game = new QuizGameEngine(quizCount, 18);
+        } catch (QuizProviderFailedException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        while (!game.isGameOver()) {
+            System.out.println(game.getQuestion());
+
+            for (int i = 0; i < game.getChoices().length; i++) {
+                System.out.printf("[%s]: %s\n", i, game.getChoices()[i]);
+            }
+
+            int choiceIndex = 0;
+            boolean isInputAccepted = false;
+            while (!isInputAccepted) {
+                choiceIndex = tryPromptInteger("Please enter a choice: ", "Only integers allowed");
+                if (choiceIndex < 0 || choiceIndex >= game.getChoices().length) {
+                    System.out.println("Only integers between 0 and 3 allowed");
+                    continue;
+                }
+                isInputAccepted = true;
+            }
+
+            boolean answeredCorrectly = game.isCorrectAnswer(choiceIndex);
+
+            if (answeredCorrectly) {
+                System.out.println("You answered correctly!");
+            } else {
+                System.out.println("You answered incorrectly!");
+            }
+            System.out.printf("The correct answer is: \"%s\"\n", game.getCorrectAnswer());
+
+            game.nextRound(answeredCorrectly);
+        }
+
+        System.out.printf("Score: %s/%s\n", game.getScore(), game.getQuizLength());
+    }
+
+    private static int tryPromptInteger(String message, String messageOnFail) {
+        Integer value = null;
+        while (value == null) {
+            try {
+                value = Integer.parseInt(prompt(message));
+            } catch (NumberFormatException e) {
+                System.out.println(messageOnFail);
+            }
+        }
+        return value;
+    }
+
+    private static String readLine() {
+        return scanner.nextLine();
+    }
+
+    private static String prompt(String message) {
+        System.out.print(message);
+        return readLine();
     }
 }
--- src/quiz/QuizGameEngine.java
+++ src/quiz/QuizGameEngine.java
@@ -47,7 +47,7 @@ public class QuizGameEngine {
         return round == quizzes.length;
     }
 
-    private int getQuizLength() {
+    public int getQuizLength() {
         return quizzes.length;
     }
 
